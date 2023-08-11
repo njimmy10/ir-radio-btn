a70fa3c2553840e19d474a7bbf3cceb4db69bf4e
njimmy10 <90354662+njimmy10@users.noreply.github.com>
2023-08-11 14:14:59 +0300

    Relying on select format

--- src/components/my-component/ir-radio-btn.tsx
+++ src/components/my-component/ir-radio-btn.tsx
@@ -1,33 +1,18 @@
-import { Component, h, State } from '@stencil/core';
+import { Component, h, Prop, Event, EventEmitter } from '@stencil/core';
+import { selectOption } from '../../common/models';
 
 @Component({
   tag: 'ir-radio-btn',
   styleUrl: 'ir-radio-btn.css',
 })
 export class MyComponent {
-  @State() data = [
-    {
-      id: 1,
-      label: 'Radio Button 1',
-      selected: false,
-    },
-    {
-      id: 2,
-      label: 'Radio Button 2',
-      selected: false,
-    },
-    {
-      id: 3,
-      label: 'Radio Button 3',
-      selected: false,
-    },
-  ];
+  @Prop({ reflect: true, mutable: true }) selectedValue = null;
+  @Prop() data: selectOption[];
+  @Event() selectChange: EventEmitter;
 
   _handleClick(item) {
-    this.data = this.data.map((i) => ({
-      ...i,
-      selected: i.id === item.id ? !i.selected : false,
-    }));
+    this.selectedValue = item.value;
+    this.selectChange.emit(this.selectedValue);
   }
 
   _renderButton(item) {
@@ -37,10 +22,10 @@ export class MyComponent {
           class="state iradio_square-red hover mr-1"
           type="radio"
           onClick={() => this._handleClick(item)}
-          value={item.id}
-          checked={item.selected}
+          value={item.value}
+          checked={this.selectedValue === item.value}
         />
-        <label>{item.label}</label>
+        <label>{item.text}</label>
       </div>
     );
   }
